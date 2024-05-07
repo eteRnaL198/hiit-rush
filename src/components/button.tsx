d0c66381ae8511e43ec69459e25906b4ad76a58e
Ikuya Nagaki <ikuya313198@gmail.com>
2024-05-07 21:17:56 +0900

    nav

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,13 @@
 type Props = {
   label: string;
+  className: string;
   onClick: () => void;
 };
 
-const Button = ({ label, onClick }: Props) => {
+const Button = ({ label, className, onClick }: Props) => {
   return (
     <button
-      className=" bg-gray-200 shadow-lg rounded-full px-8 py-4 font-bold"
+      className={` bg-gray-300 shadow-lg rounded-full px-8 py-4 font-bold ${className}`}
       onClick={onClick}
     >
       {label}
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "@/components/button";
+
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <header className="border-b-2 border-solid border-gray-300 rounded-b-3xl shadow-md">
-      <h1 className="w-5/6 mx-auto py-4 text-4xl italic">HIIT Rush🔥</h1>
-    </header>
+    <>
+      <header className="border-b-2 border-solid border-gray-300 rounded-b-3xl shadow-md">
+        <div className="flex justify-between w-5/6 mx-auto py-4">
+          <h1 className="text-3xl italic">HIIT Rush🔥</h1>
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <Image src="/hamburger.svg" alt="Menu" width={40} height={40} />
+          </button>
+        </div>
+      </header>
+      {isMenuOpen && (
+        <nav className="absolute top-0 w-full min-h-full bg-white/90 text-3xl">
+          <div className="flex flex-col gap-8 items-center py-32">
+            <Link
+              href="/"
+              className="border-b-2 border-gray-400 w-5/6 text-center py-2"
+            >
+              <button onClick={() => setIsMenuOpen(false)}>Timer</button>
+            </Link>
+            <Link
+              href="/settings"
+              className="border-b-2 border-gray-400 w-5/6 text-center py-2"
+            >
+              <button onClick={() => setIsMenuOpen(false)}>Settings</button>
+            </Link>
+            <Button
+              onClick={() => setIsMenuOpen(false)}
+              label="close"
+              className="ffont"
+            />
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
 
