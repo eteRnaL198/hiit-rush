2444a43b39f1852c096503a28adf690733e26b95
Ikuya Nagaki <ikuya313198@gmail.com>
2024-07-04 19:40:54 +0900

    feat: store data to local storage

--- src/features/timer/components/timer-settings.tsx
+++ src/features/timer/components/timer-settings.tsx
@@ -1,5 +1,7 @@
 import NumberInput from "@/components/number-input";
 import useTimerSettings from "@/features/timer/helper/useTimerSettings";
+import Button from "@/components/button";
+import { writeToStorage } from "@/services/localstorage";
 
 const TimerSettings = () => {
   const {
@@ -14,44 +16,54 @@ const TimerSettings = () => {
   } = useTimerSettings();
 
   return (
-    <div className="grid grid-cols-2 gap-4 place-items-center">
-      <label htmlFor="interval-ready">Ready</label>
-      <div className="grid grid-cols-2 gap-1">
-        <NumberInput
-          defaultValue={readyTime.toString()}
-          id="interval-ready"
-          handleValidChange={setReadyTime}
-        />
-        <p>sec</p>
+    <>
+      <div className="grid grid-cols-2 gap-4 place-items-center">
+        <label htmlFor="interval-ready">Ready</label>
+        <div className="grid grid-cols-2 gap-1">
+          <NumberInput
+            defaultValue={readyTime.toString()}
+            id="interval-ready"
+            setValidValue={setReadyTime}
+          />
+          <p>sec</p>
+        </div>
+        <label htmlFor="interval-workout">Workout</label>
+        <div className="grid grid-cols-2 gap-1">
+          <NumberInput
+            defaultValue={workoutTime.toString()}
+            id="interval-workout"
+            setValidValue={setWorkoutTime}
+          />
+          <p>sec</p>
+        </div>
+        <label htmlFor="interval-rest">Rest</label>
+        <div className="grid grid-cols-2 gap-1">
+          <NumberInput
+            defaultValue={restTime.toString()}
+            id="interval-rest"
+            setValidValue={setRestTime}
+          />
+          <p>sec</p>
+        </div>
+        <label htmlFor="interval-set">Set</label>
+        <div className="grid grid-cols-2 gap-1">
+          <NumberInput
+            defaultValue={setCount.toString()}
+            id="interval-set"
+            setValidValue={setSetCount}
+          />
+          <p>set</p>
+        </div>
       </div>
-      <label htmlFor="interval-workout">Workout</label>
-      <div className="grid grid-cols-2 gap-1">
-        <NumberInput
-          defaultValue={workoutTime.toString()}
-          id="interval-workout"
-          handleValidChange={setWorkoutTime}
+      <div className="mt-8 flex justify-center">
+        <Button
+          label="Save"
+          onClick={() => {
+            writeToStorage({ readyTime, workoutTime, restTime, setCount });
+          }}
         />
-        <p>sec</p>
       </div>
-      <label htmlFor="interval-rest">Rest</label>
-      <div className="grid grid-cols-2 gap-1">
-        <NumberInput
-          defaultValue={restTime.toString()}
-          id="interval-rest"
-          handleValidChange={setRestTime}
-        />
-        <p>sec</p>
-      </div>
-      <label htmlFor="interval-set">Set</label>
-      <div className="grid grid-cols-2 gap-1">
-        <NumberInput
-          defaultValue={setCount.toString()}
-          id="interval-set"
-          handleValidChange={setSetCount}
-        />
-        <p>set</p>
-      </div>
-    </div>
+    </>
   );
 };
 
