f2f972fa673aa6d1284df2c5c3718236ae653931
Ikuya Nagaki <ikuya313198@gmail.com>
2024-07-18 16:37:27 +0900

    feat: calendar data

--- src/features/calendar/components/calendar.tsx
+++ src/features/calendar/components/calendar.tsx
@@ -1,26 +1,39 @@
 import React from "react";
-import Calendar, { TileArgs } from "react-calendar";
+import Calendar, { OnClickFunc, TileArgs } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import useCalendar from "../helper/useCalendar";
+import Button from "@/components/button";
 
 const MyCalendar = () => {
-  const { dates } = useCalendar();
+  const { addDate, saveDates, existsDate, removeDate } = useCalendar();
 
   const tileContent = ({ date, view }: TileArgs) => {
-    if (
-      view === "month" &&
-      dates.some((d) => d.toDateString() === date.toDateString())
-    ) {
+    if (view === "month" && existsDate(date)) {
       return <span>✔</span>;
     }
   };
 
+  const handleClickDay: OnClickFunc = (value) => {
+    if (existsDate(value)) {
+      removeDate(value);
+      return;
+    }
+    addDate(value);
+  };
+
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col justify-center items-center gap-8">
       <Calendar
         calendarType="gregory"
         tileContent={tileContent}
         locale="en-GB"
+        onClickDay={handleClickDay}
+      />
+      <Button
+        label="✎ Save"
+        onClick={() => {
+          saveDates();
+        }}
       />
     </div>
   );
--- src/features/calendar/helper/useCalendar.ts
+++ src/features/calendar/helper/useCalendar.ts
@@ -2,25 +2,69 @@ import { getStoredData } from "@/services/localstorage";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { CalendarData } from "@/utils/types";
-
-const generateDates = (data: CalendarData): Date[] => {
-  return Object.entries(data).flatMap(([year, months]) =>
-    Object.entries(months).flatMap(([month, days]) =>
-      days.map((day) => new Date(Number(year), Number(month) - 1, day))
-    )
-  );
-};
+import { updateStorage } from "@/services/localstorage";
 
 const useCalendar = () => {
-  const [dates, setDates] = useState<Date[]>([]);
+  const [calendarData, setCalendarData] = useState<CalendarData>([]);
   const router = useRouter();
   useEffect(() => {
     const storedData = getStoredData<CalendarData>("calendar");
     if (!storedData) return;
-    setDates(generateDates(storedData));
+    setCalendarData(storedData);
   }, [router]);
 
-  return { dates, setDates };
+  const addDate = (date: Date): void => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // 月は0から始まるので1を足す
+    const day = date.getDate();
+
+    if (!calendarData[year]) {
+      calendarData[year] = {};
+    }
+    if (!calendarData[year][month]) {
+      calendarData[year][month] = [];
+    }
+    if (!calendarData[year][month].includes(day)) {
+      calendarData[year][month].push(day);
+      calendarData[year][month].sort((a, b) => a - b);
+    }
+  };
+
+  const removeDate = (date: Date): void => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // 月は0から始まるので1を足す
+    const day = date.getDate();
+
+    if (!calendarData[year] || !calendarData[year][month]) {
+      return;
+    }
+
+    const dayIndex = calendarData[year][month].indexOf(day);
+    if (dayIndex === -1) {
+      return;
+    }
+    calendarData[year][month].splice(dayIndex, 1);
+
+    if (calendarData[year][month].length === 0) {
+      delete calendarData[year][month];
+    }
+    if (Object.keys(calendarData[year]).length === 0) {
+      delete calendarData[year];
+    }
+  };
+
+  const existsDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // 月は0から始まるので1を足す
+    const day = date.getDate();
+    return calendarData[year]?.[month]?.includes(day) ?? false;
+  };
+
+  const saveDates = () => {
+    updateStorage<CalendarData>({ calendar: calendarData });
+  };
+
+  return { addDate, existsDate, removeDate, saveDates };
 };
 
 export default useCalendar;
